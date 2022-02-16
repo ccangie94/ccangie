89b10b2ddd07fdb349dc8ae90971243794b71cd1
ccangie94 <ccangie94@gmail.com>
2022-02-15 19:55:51 -0500

    .

--- index.js
+++
@@ -1,6 +0,0 @@
-let a = 10 
-let b = 14 
-if (a > b);
-else (b < a)
-console.log('El numero mayor es:')
-console.log(11,10);
--- index.py
+++ index.py
@@ -1,3 +1,23 @@
-a = 13
-print('Angie cumple el :')
-print(a) 
+class Personas (Object):
+    nombre = ""
+    edad = 0
+    estatura = 0.0
+    genero = ""
+
+def __init__(self, nombre, edad, estatura, genero):
+    self.nombre=nombre
+    self.edad=edad
+    self.estatura=estatura
+    self.genero=genero
+
+def caminar(self):
+    self.Caminar
+
+def hablar(self):
+    self.Hablar
+
+def saltar(self):
+    self.Saltar
+
+def es_mayor_de_edad(self):
+    self.Es_mayor_de_edad
